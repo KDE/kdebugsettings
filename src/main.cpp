5686ed464d6d2ee0877269140e88be8304f10cdc
Montel Laurent <montel@kde.org>
2015-07-27 08:09:59 +0200

    Add Application/Versioning

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@
 
 #include <QApplication>
 
+#include "config-kdebugsettings.h"
 #include "kdebugsettingsdialog.h"
 
 #include <KLocalizedString>
@@ -31,7 +32,7 @@ int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
 
-    KAboutData aboutData(QStringLiteral("kdebugsettings"), i18n("kdebugssettings"), QStringLiteral("1.1"),
+    KAboutData aboutData(QStringLiteral("kdebugsettings"), i18n("kdebugssettings"), QStringLiteral(KDEBUGSETTINGS_VERSION),
                          i18n("Configure debug settings"), KAboutLicense::GPL_V2,
                          i18n("(c) 2015 kdebugsettings authors"));
     aboutData.addAuthor(i18n("Laurent Montel"), i18n("Maintainer"), QStringLiteral("montel@kde.org"));
