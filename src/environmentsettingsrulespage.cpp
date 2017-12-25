099f812f3a175babc167955ebc7b513b8bbfa0fc
Laurent Montel <montel@kde.org>
2017-12-25 18:18:38 +0100

    Remove extra new line

--- src/environmentsettingsrulespage.cpp
+++ src/environmentsettingsrulespage.cpp
@@ -46,5 +46,5 @@ void EnvironmentSettingsRulesPage::setRules(const QString &rules)
 {
     const QString information = QStringLiteral("<qt><b>") + i18n("These rules cannot be edited with this application. You need to set them in QT_LOGGING_RULES variable directly.") + QStringLiteral("</b><qt>");
     QString newRules = rules;
-    mRichTextEdit->setHtml(information + QStringLiteral("<br><br>") + i18n("Current rules:") + QStringLiteral("<br>") + newRules.replace(QStringLiteral("\n"), QStringLiteral("<br>")));
+    mRichTextEdit->setHtml(information + i18n("Current rules:") + QStringLiteral("<br>") + newRules.replace(QStringLiteral("\n"), QStringLiteral("<br>")));
 }
