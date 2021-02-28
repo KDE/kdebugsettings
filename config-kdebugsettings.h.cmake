4310ed73fbce44e6347c3d509c1aecbd2708ac1a
Laurent Montel <montel@kde.org>
2021-02-28 10:05:04 +0100

    Add license here too

--- config-kdebugsettings.h.cmake
+++ config-kdebugsettings.h.cmake
@@ -1,1 +1,7 @@
+/*
+    SPDX-FileCopyrightText: 2015-2021 Laurent Montel <montel@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+
+*/
 #define KDEBUGSETTINGS_VERSION "${RELEASE_SERVICE_VERSION}"
