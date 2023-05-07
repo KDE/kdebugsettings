6ab602f6d4c52cd1457e471a2244b95a1f42dd87
Laurent Montel <montel@kde.org>
2023-05-07 08:28:13 +0200

    We need model here

--- src/core/loggingmanager.h
+++ src/core/loggingmanager.h
@@ -7,11 +7,14 @@
 #pragma once
 #include "libkdebugsettingscore_export.h"
 #include <QObject>
-
+class LoggingCategoryModel;
 class LIBKDEBUGSETTINGSCORE_EXPORT LoggingManager : public QObject
 {
     Q_OBJECT
 public:
     explicit LoggingManager(QObject *parent = nullptr);
     ~LoggingManager() override;
+
+private:
+    LoggingCategoryModel *mCustomCategoryModel = nullptr;
 };
