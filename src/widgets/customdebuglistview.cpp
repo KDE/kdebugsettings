238e41831631992ea89cc0fcea2cd40dc1eb0b1c
Laurent Montel <montel@kde.org>
2023-05-23 08:52:32 +0200

    Implement edit rule

--- src/widgets/customdebuglistview.cpp
+++ src/widgets/customdebuglistview.cpp
@@ -7,6 +7,7 @@
 
 #include "customdebuglistview.h"
 #include "configurecustomsettingdialog.h"
+#include "kdebugsettingsutil.h"
 #include "model/customdebugproxymodel.h"
 #include "model/loggingcategorymodel.h"
 
@@ -85,7 +86,32 @@ void CustomDebugListView::slotEditRule(const QModelIndex &index)
     if (dlg->exec()) {
         const QString ruleStr = dlg->rule();
         if (!ruleStr.isEmpty()) {
-            // item->setText(dlg->rule());
+            const KDebugSettingsUtil::LineLoggingQtCategory cat = KDebugSettingsUtil::parseLineLoggingQtCategory(ruleStr);
+            if (cat.isValid()) {
+                LoggingCategory tmp;
+                tmp.categoryName = cat.logName;
+                tmp.enabled = cat.enabled;
+                switch (cat.type) {
+                case KDebugSettingsUtil::LineLoggingQtCategory::Unknown:
+                    break;
+                case KDebugSettingsUtil::LineLoggingQtCategory::Info:
+                    tmp.loggingType = LoggingCategory::Info;
+                    break;
+                case KDebugSettingsUtil::LineLoggingQtCategory::Warning:
+                    tmp.loggingType = LoggingCategory::Warning;
+                    break;
+                case KDebugSettingsUtil::LineLoggingQtCategory::Debug:
+                    tmp.loggingType = LoggingCategory::Debug;
+                    break;
+                case KDebugSettingsUtil::LineLoggingQtCategory::Critical:
+                    tmp.loggingType = LoggingCategory::Critical;
+                    break;
+                case KDebugSettingsUtil::LineLoggingQtCategory::All:
+                    tmp.loggingType = LoggingCategory::All;
+                    break;
+                }
+                mLoggingCategoryModel->insertCategories({tmp});
+            }
         }
     }
     delete dlg;
