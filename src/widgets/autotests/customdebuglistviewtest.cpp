74f2f9d6639cb1198d50f569654ab8d3611f8824
Laurent Montel <montel@kde.org>
2023-05-24 07:31:15 +0200

    Add customdebuglistviewtest

---
+++ src/widgets/autotests/customdebuglistviewtest.cpp
@@ -0,0 +1,23 @@
+/*
+    SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "customdebuglistviewtest.h"
+#include "customdebuglistview.h"
+#include <QTest>
+
+QTEST_MAIN(CustomDebugListViewTest)
+
+CustomDebugListViewTest::CustomDebugListViewTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void CustomDebugListViewTest::shouldHaveDefaultValues()
+{
+    CustomDebugListView w;
+    QCOMPARE(w.contextMenuPolicy(), Qt::CustomContextMenu);
+    QCOMPARE(w.selectionMode(), QAbstractItemView::ExtendedSelection);
+}
---
+++ src/widgets/autotests/customdebuglistviewtest.h
@@ -0,0 +1,19 @@
+/*
+    SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QObject>
+
+class CustomDebugListViewTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit CustomDebugListViewTest(QObject *parent = nullptr);
+    ~CustomDebugListViewTest() override = default;
+private Q_SLOTS:
+    void shouldHaveDefaultValues();
+};
