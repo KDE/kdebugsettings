bbe6f2876d1fcd57b801cd6eb19e2e80dafd5f1d
Fushan Wen <qydwhotmail@gmail.com>
2023-10-01 12:15:31 +0800

    appiumtests: port away from deprecated desired_capabilities
    `AppiumOptions` replaces it

--- src/seleniumtests/kdebugsettings.py
+++ src/seleniumtests/kdebugsettings.py
@@ -3,28 +3,30 @@
 # SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
 
 import unittest
+
 from appium import webdriver
+from appium.options.common.base import AppiumOptions
 from appium.webdriver.common.appiumby import AppiumBy
-import selenium.common.exceptions
-from selenium.webdriver.support.ui import WebDriverWait
+
 
 class SmokeTests(unittest.TestCase):
+
     @classmethod
-    def setUpClass(self):
-        desired_caps = {}
-        desired_caps["app"] = "org.kde.kdebugsettings.desktop"
-        self.driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', desired_capabilities=desired_caps)
-        self.driver.implicitly_wait = 10
+    def setUpClass(cls):
+        options = AppiumOptions()
+        options.set_capability("app", "org.kde.kdebugsettings.desktop")
+        cls.driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', options=options)
 
     @classmethod
     def tearDownClass(self):
         self.driver.quit()
 
     def setUp(self):
-            pass
+        pass
 
     def test_addTrainTrip(self):
         self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="QApplication.KDebugSettingsDialog.buttonbox.insert_button").click()
+
 
 if __name__ == '__main__':
     unittest.main()
