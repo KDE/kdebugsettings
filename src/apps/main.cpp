43cfeac514320c4ee80ddfddcaff09c1ce2d482a
Laurent Montel <montel@kde.org>
2024-05-15 07:55:15 +0200

    Add contect

--- src/apps/main.cpp
+++ src/apps/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
                          i18n("Configure debug settings"),
                          KAboutLicense::GPL_V2,
                          i18n("(c) 2015-%1 kdebugsettings authors", QStringLiteral("2024")));
-    aboutData.addAuthor(i18n("Laurent Montel"), i18n("Maintainer"), QStringLiteral("montel@kde.org"));
+    aboutData.addAuthor(i18nc("@info:credit", "Laurent Montel"), i18n("Maintainer"), QStringLiteral("montel@kde.org"));
     QApplication::setWindowIcon(QIcon::fromTheme(QStringLiteral("debug-run")));
     KAboutData::setApplicationData(aboutData);
 
