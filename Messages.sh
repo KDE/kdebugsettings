56648c22931771879db675d08e60e1e0965ab750
Laurent Montel <montel@kde.org>
2026-07-19 18:15:07 +0200

    Extract qml i18n string

--- Messages.sh
+++ Messages.sh
@@ -2,4 +2,4 @@
 # SPDX-FileCopyrightText: 2015-2026 Laurent Montel <montel@kde.org>
 # SPDX-FileCopyrightText: 2023 Luigi Toscano <luigi.toscano@tiscali.it>
 # SPDX-License-Identifier: BSD-3-Clause
-$XGETTEXT `find src/ -name '*.cpp' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/kdebugsettings.pot
+$XGETTEXT `find src/ -name '*.cpp' -o -name '*.qml' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/kdebugsettings.pot
