1436a13bc21d478e422a6564f72cc335a2ab6720
Laurent Montel <montel@kde.org>
2026-09-18 19:51:36 +0200

    avoid to show html text

--- src/widgets/environmentsettingsrulespage.cpp
+++ src/widgets/environmentsettingsrulespage.cpp
@@ -29,7 +29,7 @@ void EnvironmentSettingsRulesPage::setRules(const QString &rules)
 {
     const QString information =
         "<qt><b>"_L1 + i18n("These rules cannot be edited with this application. You need to set them in QT_LOGGING_RULES variable directly.") + "</b></qt>"_L1;
-    QString newRules = rules;
+    QString newRules = rules.toHtmlEscaped();
     mRichTextEdit->setHtml(information + i18n("Current rules:") + u"<br>"_s + newRules.replace(u'\n', "<br>"_L1));
 }
 
